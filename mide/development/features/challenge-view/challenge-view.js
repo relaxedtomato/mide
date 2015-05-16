dcf86f44585cea2b7a1df6d86c56f74126908ff2
Albert Yin <albyin@gmail.com>
2015-05-16 13:31:12 -0400

    merge conflcits

--- mide/development/features/challenge-view/challenge-view.js
+++ mide/development/features/challenge-view/challenge-view.js
@@ -10,7 +10,7 @@ app.config(function($stateProvider){
 	});
 });
 
-app.controller('ChallengeViewCtrl', function($scope, ChallengeFactory, $state, $ionicSlideBoxDelegate){
+app.controller('ChallengeViewCtrl', function($scope, ChallengeFactory, $state, $ionicSlideBoxDelegate, $ionicModal){
 
 	//Controls Slide
 	$scope.slideHasChallenged = function(index){
@@ -24,5 +24,29 @@ app.controller('ChallengeViewCtrl', function($scope, ChallengeFactory, $state, $
 	// 	$scope.challenge = ChallengeFactory.getProblem();
 	// });
 
-
+	$ionicModal.fromTemplateUrl('my-modal.html', {
+	  scope: $scope,
+	  animation: 'slide-in-up'
+	}).then(function(modal) {
+	  $scope.modal = modal;
+	});
+	$scope.openModal = function() {
+	  $scope.modal.show();
+	};
+	$scope.closeModal = function() {
+	  $scope.modal.hide();
+	};
+	//Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+	  $scope.modal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+	  // Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+	  // Execute action
+	});
+	
 });
