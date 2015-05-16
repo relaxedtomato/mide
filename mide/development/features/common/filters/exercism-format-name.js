e6a4974dba335b5c74077b5e01e2cd6f0b085df2
Tony <terimyaki@users.noreply.github.com>
2015-05-16 15:51:20 -0400

    added exercism and madethe code editors into directives

---
+++ mide/development/features/common/filters/exercism-format-name.js
@@ -0,0 +1,7 @@
+app.filter('nameformat', function(){
+	return function(text){
+		return 'Exercism - ' + text.split('-').map(function(el){
+			return el.charAt(0).toUpperCase() + el.slice(1);
+		}).join(' ');
+	};
+});
