c24618f979b61b5b5b5ff4150adc98c460aa9074
Albert Yin <albyin@gmail.com>
2015-05-18 18:22:43 -0400

    commit changes before merge

--- mide/development/features/challenge-code/challenge-code.js
+++ mide/development/features/challenge-code/challenge-code.js
@@ -7,75 +7,58 @@ app.config(function($stateProvider){
 				controller : 'ChallengeCodeCtrl'
 			}
 		}
-		// ,
-		// onEnter : function(ChallengeFactory, $state){
-		// 	if(ChallengeFactory.getProblem().length === 0){
-		// 		$state.go('challenge.view');
-		// 	}
-		// }
 	});
 });
 
 
 app.controller('ChallengeCodeCtrl', function($scope, $state, $rootScope, ChallengeFactory, ChallengeFooterFactory, $ionicPopup, $localstorage){
 
-	setTimeout(function (){
-		// $scope.keyboardVis = window.cordova.plugins.Keyboard.isVisible;
-		// 	console.log("cordova isvis", window.cordova.plugins.Keyboard.isVisible);
-		// 	console.log("$scope keyboardVis", $scope.keyboardVis);
-
-
-		// if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-		//   window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		//   window.cordova.plugins.Keyboard.disableScroll(true);
-		// }
-	}, 50);
 
 	$scope.footerMenu = ChallengeFooterFactory.getFooterMenu();
 
 
 	//Challenge Submit
-	$scope.text = ChallengeFactory.getSubmission() || 'text';
+	$scope.text = "placeholder";
 
 	//initialize CodeMirror
-	var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('code'), {
-		lineNumbers : true,
-		mode: 'javascript',
-		autofocus : true,
-		theme : 'twilight',
-		lineWrapping: true
-	});
+	// var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('code'), {
+	// 	lineNumbers : true,
+	// 	mode: 'javascript',
+	// 	autofocus : true,
+	// 	theme : 'twilight',
+	// 	lineWrapping: true
+	// });
 
-	myCodeMirror.replaceSelection($scope.text);
+	// myCodeMirror.replaceSelection($scope.text);
 
-	$scope.updateText = function(){
-		$scope.text = myCodeMirror.getValue();
-		//check if digest is in progress
-		if(!$scope.$$phase) {
-		  $scope.$apply();
-		}
-	};
+	// $scope.updateText = function(){
+	// 	$scope.text = myCodeMirror.getValue();
+	// 	//check if digest is in progress
+	// 	if(!$scope.$$phase) {
+	// 	  $scope.$apply();
+	// 	}
+	// };
 
-	$scope.insertFunc = function(param){
-		//given a param, will insert characters where cursor is
-		console.log("inserting: ", param);
-		myCodeMirror.replaceSelection(param);
-		// window.cordova.plugins.Keyboard.show();
-		myCodeMirror.focus();
-	};
+	// $scope.insertFunc = function(param){
+	// 	//given a param, will insert characters where cursor is
+	// 	console.log("inserting: ", param);
+	// 	myCodeMirror.replaceSelection(param);
+	// 	// window.cordova.plugins.Keyboard.show();
+	// 	myCodeMirror.focus();
+	// };
 
-    myCodeMirror.on("change", function (myCodeMirror, changeObj){
-    	$scope.updateText();
-    });
+ //    myCodeMirror.on("change", function (myCodeMirror, changeObj){
+ //    	$scope.updateText();
+ //    });
 
-    window.addEventListener("native.keyboardshow", function (){
-    	$scope.keyboardVis = true;
-    	$scope.$apply();
-    });
-    window.addEventListener("native.keyboardhide", function (){
-    	$scope.keyboardVis = false;
-    	$scope.$apply();
-    });
+ //    window.addEventListener("native.keyboardshow", function (){
+ //    	$scope.keyboardVis = true;
+ //    	$scope.$apply();
+ //    });
+ //    window.addEventListener("native.keyboardhide", function (){
+ //    	$scope.keyboardVis = false;
+ //    	$scope.$apply();
+ //    });
 
 	$scope.buttons = {
 		compile : 'Compile',
@@ -101,15 +84,14 @@ app.controller('ChallengeCodeCtrl', function($scope, $state, $rootScope, Challen
 	};
 
 	$scope.saveChallenge = function(){
-		$localstorage.set("codeContents", $scope.text);
+		console.log("save scope.text", $scope.text);
+		$localstorage.set("testing", $scope.text);
 	};
 
 	$scope.getSaved = function(){
+		console.log("save scope.text", $scope.text);
 		console.log("entered getsaved func");
-		$scope.text = $localstorage.get("codeContents");
-		if(!$scope.$$phase) {
-		  $scope.$apply();
-		}
+		$scope.text = $localstorage.get("testing");
 	};
 
 });
