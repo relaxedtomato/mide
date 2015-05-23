e5c853a07e4f0fd3af770048b1360a58d825c319
Richard Bansal <richard.bansal@gmail.com>
2015-05-23 17:09:37 -0400

    updates for heroku deploy

--- server/app/api/github/index.js
+++ server/app/api/github/index.js
@@ -1,13 +1,14 @@
 var github=require('github');
 var githubInstance = new github({version: "3.0.0"});
 var gist = require('../../../env/gist.js')
+var production = require('../../../env/production.js')
 //githubInstance = bluebird.promisifiy
 
 //TODO: You need to track total calls per hour to avoid rate limiting, or per user
 githubInstance.authenticate({ //TODO: Shift to library instead of here, to only complete once on server load
     type: "oauth",
-    key: gist.CLIENT,
-    secret: gist.CLIENT_SECRET
+    key: production.GIST_CLIENT || gist.CLIENT,
+    secret: production.GIST_SECRET || gist.CLIENT_SECRET
 });
 
 module.exports = githubInstance;
